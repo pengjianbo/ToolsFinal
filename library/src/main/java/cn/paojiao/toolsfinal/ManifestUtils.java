d882dc19617484a436f8f26d1d39cbdae0441b02
彭建波 <pengjianbosoft@gmail.com>
2015-09-18 17:24:11 +0800

    提交项目代码

---
+++ library/src/main/java/cn/paojiao/toolsfinal/ManifestUtils.java
@@ -0,0 +1,86 @@
+package cn.paojiao.toolsfinal;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Desction:Android Manifest工具类
+ * Author:pengjianbo
+ * Date:15/9/17 下午4:38
+ */
+public class ManifestUtils {
+
+    /**
+     * 获得渠道号
+     * @param context
+     * @return
+     */
+    public static String getChannelNo(Context context) {
+        String name = context.getPackageName();
+        ApplicationInfo appInfo;
+        String msg = "";
+        try {
+            appInfo = context.getPackageManager().getApplicationInfo(name,
+                    PackageManager.GET_META_DATA);
+            msg = appInfo.metaData.getString("UMENG_CHANNEL");
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (StringUtils.isEmpty(msg)) {
+            return "";
+        }
+
+        return msg;
+    }
+
+    /**
+     * 获得apk版本号
+     * @param context
+     * @return
+     */
+    public static String getVersionName(Context context) {
+        String version = "";
+        // 获取packagemanager的实例
+        PackageManager packageManager = context.getPackageManager();
+        // getPackageName()是你当前类的包名，0代表是获取版本信息
+        PackageInfo packInfo;
+        try {
+            packInfo = packageManager.getPackageInfo(context.getPackageName(),
+                    0);
+            version = packInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (StringUtils.isEmpty(version)) {
+            version = "";
+        }
+
+        return version;
+    }
+
+    /**
+     * 获得apk版本号
+     * @param context
+     * @return
+     */
+    public static int getVersionCode(Context context) {
+        int versionCode = 0;
+        // 获取packagemanager的实例
+        PackageManager packageManager = context.getPackageManager();
+        // getPackageName()是你当前类的包名，0代表是获取版本信息
+        PackageInfo packInfo;
+        try {
+            packInfo = packageManager.getPackageInfo(context.getPackageName(),
+                    0);
+            versionCode = packInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return versionCode;
+    }
+}
