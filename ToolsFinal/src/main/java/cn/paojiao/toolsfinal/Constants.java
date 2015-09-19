6098a3ac5610dcf48c6a4765891753de977f3287
彭建波 <pengjianbosoft@gmail.com>
2015-09-19 18:15:00 +0800

    整理工具类

---
+++ ToolsFinal/src/main/java/cn/paojiao/toolsfinal/Constants.java
@@ -0,0 +1,10 @@
+package cn.paojiao.toolsfinal;
+
+/**
+ * Desction:
+ * Author:pengjianbo
+ * Date:15/9/17 下午4:13
+ */
+public class Constants {
+    public static boolean DEBUG = BuildConfig.DEBUG;
+}
---
+++ ToolsFinal/src/main/java/cn/paojiao/toolsfinal/DeviceUtils.java
@@ -0,0 +1,483 @@
+package cn.paojiao.toolsfinal;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Environment;
+import android.os.StatFs;
+import android.os.Vibrator;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.inputmethod.InputMethodManager;
+import java.io.File;
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Desction:设备相关工具类
+ * Author:pengjianbo
+ * Date:15/9/17 下午4:21
+ */
+public class DeviceUtils {
+
+    /**
+     * 判断SDCard是否可用
+     */
+    public static boolean existSDCard() {
+        if (android.os.Environment.getExternalStorageState().equals(
+                android.os.Environment.MEDIA_MOUNTED)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 获取本机IP地址
+     * @return
+     */
+    public static String getLocalIPAddress() {
+
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface
+                    .getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf
+                        .getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            return "0.0.0.0";
+        }
+        return "0.0.0.0";
+    }
+
+    /**
+     * 多个SD卡时 取外置SD卡
+     * @return
+     */
+    public static String getExternalStorageDirectory() {
+        // 参考文章
+        // http://blog.csdn.net/bbmiku/article/details/7937745
+        Map<String, String> map = System.getenv();
+        String[] values = new String[map.values().size()];
+        map.values().toArray(values);
+        String path = values[values.length - 1];
+        if (path.startsWith("/mnt/") && !Environment.getExternalStorageDirectory()
+                .getAbsolutePath()
+                .equals(path)) {
+            return path;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 获取可用空间大小
+     * @return
+     */
+    public static long getAvailaleSize(){
+        if ( !existSDCard() ) {
+            return 0l;
+        }
+        File path = Environment.getExternalStorageDirectory(); //取得sdcard文件路径
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSize();
+        long availableBlocks = stat.getAvailableBlocks();
+        return availableBlocks * blockSize;
+    }
+
+    /**
+     * 获取SD大小
+     * @return
+     */
+    public static long getAllSize(){
+        if ( !existSDCard() ) {
+            return 0l;
+        }
+        File path = Environment.getExternalStorageDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSize();
+        long availableBlocks = stat.getBlockCount();
+        return availableBlocks * blockSize;
+    }
+
+    public static boolean isOnline(Context context) {
+        try {
+            ConnectivityManager cm =
+                    (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo ni = cm.getActiveNetworkInfo();
+            return ni != null ? ni.isConnectedOrConnecting() : false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 服务是否运行
+     * @param mContext
+     * @param className
+     * @return
+     */
+    public static boolean isServiceRunning(Context mContext,String className) {
+        boolean isRunning = false;
+        ActivityManager activityManager = (ActivityManager)
+                mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> serviceList
+                = activityManager.getRunningServices(Integer.MAX_VALUE);
+        if (serviceList.size() == 0) {
+            return false;
+        }
+        for (int i=0; i<serviceList.size(); i++) {
+            if (serviceList.get(i).service.getClassName().equals(className) == true) {
+                isRunning = true;
+                break;
+            }
+        }
+        return isRunning;
+    }
+
+    /**
+     * 进程是否运行
+     */
+    public static boolean isProessRunning(Context context, String proessName) {
+        boolean isRunning = false;
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+
+        List<ActivityManager.RunningAppProcessInfo> lists = am.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo info : lists) {
+            if (info.processName.equals(proessName)) {
+                isRunning = true;
+                return isRunning;
+            }
+        }
+
+        return isRunning;
+    }
+
+    /**
+     * 获取IMEI
+     * @param context
+     * @return
+     */
+    public static String getIMEI(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = tm.getDeviceId();
+        if ( StringUtils.isEmpty(imei) ) {
+            imei = "";
+        }
+
+        return imei;
+    }
+
+    /**
+     * 获取MAC地址
+     * @param context
+     * @return
+     */
+    public static String getMac(Context context) {
+        WifiManager wifi = (WifiManager) context
+                .getSystemService(Context.WIFI_SERVICE);
+        WifiInfo info = wifi.getConnectionInfo();
+        String mac = info.getMacAddress();
+        if ( StringUtils.isEmpty(mac) ) {
+            mac = "";
+        }
+        return mac;
+    }
+
+    /**
+     * 获取UDID
+     * @param context
+     * @return
+     */
+    public static String getUDID(Context context) {
+        String udid = Settings.Secure.getString(context.getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+        if (StringUtils.isEmpty(udid) || udid.equals("9774d56d682e549c")
+                || udid.length() < 15) {
+            SecureRandom random = new SecureRandom();
+            udid = new BigInteger(64, random).toString(16);
+        }
+
+        if ( StringUtils.isEmpty(udid) ) {
+            udid = "";
+        }
+
+        return udid;
+    }
+
+    /**
+     * 震动
+     * @param context
+     * @param duration
+     */
+    public static void vibrate(Context context, long duration) {
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        long[] pattern = {
+                0, duration
+        };
+        vibrator.vibrate(pattern, -1);
+    }
+
+    /**
+     * 获取最后一次拍照的图片
+     * @param context
+     * @return
+     */
+    public static String getLatestCameraPicture(Context context) {
+
+        if ( !existSDCard() ) {
+            return null;
+        }
+
+        String[] projection = new String[] { MediaStore.Images.ImageColumns._ID, MediaStore.Images.ImageColumns.DATA,
+                MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME, MediaStore.Images.ImageColumns.DATE_TAKEN,
+                MediaStore.Images.ImageColumns.MIME_TYPE };
+        Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, null,
+                MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC");
+        if (cursor.moveToFirst()) {
+            String path = cursor.getString(1);
+            return path;
+        }
+        return null;
+    }
+
+    /**
+     * 获取手机大小（分辨率）
+     *
+     * @param activity
+     * @return
+     */
+    public static DisplayMetrics getScreenPix(Activity activity) {
+        // DisplayMetrics 一个描述普通显示信息的结构，例如显示大小、密度、字体尺寸
+        DisplayMetrics displaysMetrics = new DisplayMetrics();
+        // 获取手机窗口的Display 来初始化DisplayMetrics 对象
+        // getManager()获取显示定制窗口的管理器。
+        // 获取默认显示Display对象
+        // 通过Display 对象的数据来初始化一个DisplayMetrics 对象
+        activity.getWindowManager().getDefaultDisplay()
+                .getMetrics(displaysMetrics);
+        return displaysMetrics;
+    }
+
+    /**
+     * 复制到剪切板
+     * @param context
+     * @param content
+     */
+    @TargetApi(11)
+    public static void coptyToClipBoard(Context context, String content) {
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+            ClipboardManager clipboard = (ClipboardManager)context.getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("label", content);
+            clipboard.setPrimaryClip(clip);
+        } else {
+            android.text.ClipboardManager clipboard = (android.text.ClipboardManager)context.getSystemService(Context.CLIPBOARD_SERVICE);
+            clipboard.setText(content);
+        }
+    }
+
+    /**
+     * 获取非系统应用包名
+     * @param context
+     * @return
+     */
+    public static List<String> getAppPackageNamelist(Context context) {
+        List<String> packList = new ArrayList<>();
+        PackageManager pm = context.getPackageManager();
+        List<PackageInfo> packinfos = pm.getInstalledPackages(0);
+        for(PackageInfo packinfo:packinfos){
+            String packname = packinfo.packageName;
+            packList.add(packname);
+        }
+
+        return packList;
+    }
+
+    /**
+     * 判断某个应用是否已经安装
+     * @param context 上下文
+     * @param packageName 包名
+     * @return 是否已经安装
+     */
+    public static boolean isAppInstall(Context context,String packageName){
+        // 获取packagemanager
+        final PackageManager packageManager = context.getPackageManager();
+        // 获取所有已安装程序的包信息
+        List<PackageInfo> packageInfos = packageManager.getInstalledPackages(0);
+        // 用于存储所有已安装程序的包名
+        List<String> packageNames = new ArrayList<String>();
+        // 从pinfo中将包名字逐一取出，压入pName list中
+        if (packageInfos != null) {
+            for (int i = 0; i < packageInfos.size(); i++) {
+                String packName = packageInfos.get(i).packageName;
+                packageNames.add(packName);
+            }
+        }
+        // 判断packageNames中是否有目标程序的包名，有TRUE，没有FALSE
+        return packageNames.contains(packageName);
+    }
+
+    public static int dip2px(Context context, float dipValue){
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(dipValue * scale + 0.5f);
+    }
+
+    public static int px2dip(Context context, float pxValue){
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 判断是否有软控制键（手机底部几个按钮）
+     * @param activity
+     * @return
+     */
+    public boolean isSoftKeyAvail(Activity activity) {
+        final boolean[] isSoftkey = {false};
+        final View activityRootView = (activity).getWindow().getDecorView().findViewById(android.R.id.content);
+        activityRootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                int rootViewHeight = activityRootView.getRootView().getHeight();
+                int viewHeight = activityRootView.getHeight();
+                int heightDiff = rootViewHeight - viewHeight;
+                if (heightDiff > 100) { // 99% of the time the height diff will be due to a keyboard.
+                    isSoftkey[0] = true;
+                }
+            }
+        });
+        return isSoftkey[0];
+    }
+
+    /**
+     * 获取statusbar高度
+     * @param context
+     * @return
+     */
+    public static int getStatusBarHeight(Context context){
+        int result=0;
+        int resourceId= context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if(resourceId >0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+
+        return result;
+    }
+
+
+    /**
+     *
+     * @Title: startActivityForPackage
+     * @Description: TODO(通过)
+     * @param @param context
+     * @param @param packageName   包名
+     * @return void    返回类型
+     * @throws
+     */
+    @SuppressLint("NewApi")
+    public static boolean startActivityForPackage(Context context,String packageName){
+        PackageInfo pi = null;
+        try {
+            pi =context. getPackageManager().getPackageInfo(packageName, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        resolveIntent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        //		resolveIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        resolveIntent.setPackage(pi.packageName);
+
+        List<ResolveInfo> apps = context.getPackageManager().queryIntentActivities(resolveIntent, 0);
+
+        ResolveInfo ri = apps.iterator().next();
+        if (ri != null ) {
+            String packageName1 = ri.activityInfo.packageName;
+            String className = ri.activityInfo.name;
+
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+
+            ComponentName cn = new ComponentName(packageName1, className);
+
+            intent.setComponent(cn);
+            context.startActivity(intent);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 隐藏键盘
+     * ：强制隐藏
+     * @param context
+     */
+    public static void hideInputSoftFromWindowMethod(Context context,View view){
+        try {
+            InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        } catch (Exception e) {
+            Logger.e(e);
+        }
+    }
+
+    /**
+     * 显示输入法
+     * @param context
+     * @param view
+     */
+    public static void showInputSoftFromWindowMethod(Context context,View view){
+        try {
+            InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(view, InputMethodManager.SHOW_FORCED);
+        } catch (Exception e) {
+            Logger.e(e);
+        }
+    }
+
+    /**
+     * 判断输入负是否处于激活状态
+     * @param context
+     * @return
+     */
+    public static boolean isActiveSoftInput(Context context) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        return imm.isActive();
+    }
+}
