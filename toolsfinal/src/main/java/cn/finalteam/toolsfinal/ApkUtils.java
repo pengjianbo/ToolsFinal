baae5eeb15c38499001da846db713b72f52a5f5b
彭建波 <pengjianbosoft@gmail.com>
2015-10-31 11:23:55 +0800

    添加卸载方法

--- toolsfinal/src/main/java/cn/finalteam/toolsfinal/ApkUtils.java
+++ toolsfinal/src/main/java/cn/finalteam/toolsfinal/ApkUtils.java
@@ -21,10 +21,29 @@ import java.util.zip.ZipFile;
  */
 public class ApkUtils {
 
+    /**
+     * 安装一个apk文件
+     * @param context
+     * @param uriFile
+     */
     public static void install(Context context, File uriFile) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setDataAndType(Uri.fromFile(uriFile), "application/vnd.android.package-archive");
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+    /**
+     * 卸载一个app
+     * @param context
+     * @param packageName
+     */
+    public static void uninstall(Context context, String packageName) {
+        //通过程序的包名创建URI
+        Uri packageURI = Uri.parse("package:" + packageName);
+        //创建Intent意图
+        Intent intent = new Intent(Intent.ACTION_DELETE,packageURI);
+        //执行卸载程序
         context.startActivity(intent);
     }
 
