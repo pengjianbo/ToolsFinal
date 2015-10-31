74393f14792887bf32b540a758193c564dfd5e75
彭建波 <pengjianbosoft@gmail.com>
2015-10-31 11:26:25 +0800

    添加md5工具

---
+++ toolsfinal/src/main/java/cn/finalteam/toolsfinal/MD5Utils.java
@@ -0,0 +1,75 @@
+package cn.finalteam.toolsfinal;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Desction:MD5工具
+ * Author:pengjianbo
+ * Date:15/10/31 上午10:12
+ */
+public class MD5Utils {
+    // 全局数组
+    private final static String[] strDigits = {
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"
+    };
+
+    /**
+     * 返回形式为数字跟字符串
+     * @param bByte
+     * @return
+     */
+    private static String byteToArrayString(byte bByte) {
+        int iRet = bByte;
+        if (iRet < 0) {
+            iRet += 256;
+        }
+        int iD1 = iRet / 16;
+        int iD2 = iRet % 16;
+        return strDigits[iD1] + strDigits[iD2];
+    }
+
+    /**
+     * 返回形式只为数字
+     * @param bByte
+     * @return
+     */
+    private static String byteToNum(byte bByte) {
+        int iRet = bByte;
+        if (iRet < 0) {
+            iRet += 256;
+        }
+        return String.valueOf(iRet);
+    }
+
+    /**
+     * 转换字节数组为16进制字串
+     * @param bByte
+     * @return
+     */
+    private static String byteToString(byte[] bByte) {
+        StringBuffer sBuffer = new StringBuffer();
+        for (int i = 0; i < bByte.length; i++) {
+            sBuffer.append(byteToArrayString(bByte[i]));
+        }
+        return sBuffer.toString();
+    }
+
+    /**
+     * 获取MD5编码
+     * @param source
+     * @return
+     */
+    public static String getMD5Code(String source) {
+        String resultString = null;
+        try {
+            resultString = new String(source);
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            // md.digest() 该函数返回值为存放哈希值结果的byte数组
+            resultString = byteToString(md.digest(source.getBytes()));
+        } catch (NoSuchAlgorithmException ex) {
+            ex.printStackTrace();
+        }
+        return resultString;
+    }
+}
